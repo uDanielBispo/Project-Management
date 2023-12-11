2f01b265140792b9466520860cca9631a6abf95e
Daniel Bispo <danielbispo3015@hotmail.com>
2023-12-10 21:18:20 -0300

    Trabalhando nas novas paginas/links e criando suas rotas com react router dom, também adicionados estilos em styles.modules.css

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/home';
+import Company from './components/pages/company';
+import Contact from './components/pages/contact';
+import NewProject from './components/pages/newproject';
+
+import Container from './components/layout/container';
 
 function App() {
   return (
-    <div className="App">
+      <Router>
+        <ul>
+          <Link to='/'>Home</Link >
+          <Link to='/contact'>Contact</Link>
+          <Link to='company'>Company</Link >
+          <Link to='/newproject'>New Project</Link >
+        </ul>
 
-    </div>
+          <Container customClass='min-height'>
+            <Routes>
+              <Route path='/' element={<Home />}/>
+              <Route path='/contact' element={<Contact />}/>
+              <Route path='/company' element={<Company />}/>
+              <Route path='/newproject' element={<NewProject />}/>
+            </Routes>
+          </Container>
+
+        <p>Footer</p>
+      </Router>
   );
 }
 
