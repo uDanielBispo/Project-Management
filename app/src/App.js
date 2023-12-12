5afacd264c936586681c0384e953a8985aace60a
Daniel Bispo <danielbispo3015@hotmail.com>
2023-12-11 21:45:04 -0300

    Criando home

--- app/src/App.js
+++ app/src/App.js
@@ -3,30 +3,28 @@ import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/pages/home';
 import Company from './components/pages/company';
 import Contact from './components/pages/contact';
-import NewProject from './components/pages/newproject';
+import Projects from './components/pages/projects';
+
+import NavBar from './components/layout/navbar';
+import Footer from './components/layout/footer';
+
 
 import Container from './components/layout/container';
 
 function App() {
   return (
       <Router>
-        <ul>
-          <Link to='/'>Home</Link >
-          <Link to='/contact'>Contact</Link>
-          <Link to='company'>Company</Link >
-          <Link to='/newproject'>New Project</Link >
-        </ul>
+        <NavBar />
 
           <Container customClass='min-height'>
             <Routes>
               <Route path='/' element={<Home />}/>
               <Route path='/contact' element={<Contact />}/>
+              <Route path='/projects' element={<Projects />}/>
               <Route path='/company' element={<Company />}/>
-              <Route path='/newproject' element={<NewProject />}/>
             </Routes>
           </Container>
-
-        <p>Footer</p>
+        <Footer />
       </Router>
   );
 }
